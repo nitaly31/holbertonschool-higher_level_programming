c53825efb510665953ef1695234960255401e487
Geraldine <73443677+nitaly31@users.noreply.github.com>
2021-09-13 20:27:26 -0500

    Delete 1-element_at.py

--- 1-element_at.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return (None)
-    length = len(my_list)
-    if idx > length - 1:
-        return (None)
-    return(my_list[idx])
