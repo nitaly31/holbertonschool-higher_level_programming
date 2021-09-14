d2591233ec9b3131b9c58ff2821a215cd0ad15f5
nitaly31 <geraldine.nitaly@gmail.com>
2021-09-13 18:45:35 -0700

    reverse command modification

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return none
-    my_list.reverse()
-    for i in my_list:
+    for i in reversed.(my_list):
         print('{:d}'.format(i))
