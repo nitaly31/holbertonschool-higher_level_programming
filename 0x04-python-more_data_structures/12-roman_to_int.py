b0581615c2582041e25ff018a1d61630b005cac0
nitaly31 <geraldine.nitaly@gmail.com>
2021-09-17 17:17:01 -0700

    Function that changes the roman number to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,16 +3,10 @@ def roman_to_int(roman_string):
     if isinstance(roman_string, str) != True or roman_string == None:
         return 0
     r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    r_sum = 0
-    if type(roman_string) != str or roman_string is None:
-        return r_sum
-    if len(roman_string) == 1:
-        return r.get(roman_string[0])
-    for i in range(len(roman_string) - 1):
-        if r.get(roman_string[i]) < r.get(roman_string[i -1]):
-            r_sum += r.get(roman_string[i]) * -1
+    r_int = 0
+    for i in range(len(roman_string)):
+        if i > 0 and r[roman_string[i]] > r[roman_string[i - 1]]:
+            r_int += r[roman_string[i]] - 2 * r[roman_string[i - 1]]
         else:
-            r_sum += r.get(roman_string[i])
-        if i == len(roman_string) - 2:
-            r_sum += r.get(roman_string[i + 1])
-    return r_sum
+            r_int += r[roman_string[i]]
+    return r_int
