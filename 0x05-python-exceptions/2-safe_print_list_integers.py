b6dd008ebf590deb89f8961b0e7ab2c56615e4f7
nitaly31 <geraldine.nitaly@gmail.com>
2021-09-27 17:50:27 -0700

    delete comments

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,10 +5,10 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-        except TypeError:  #Se genera cuando una operación o función se
-            pass           #aplica a un objeto de tipo inapropiado.
-        except ValueError: #Se genera cuando una función obtiene un
-            pass           #argumento de tipo correcto pero valor incorrecto.
+        except TypeError:
+            pass
+        except ValueError:
+            pass
         else:
             count += 1
     print()
