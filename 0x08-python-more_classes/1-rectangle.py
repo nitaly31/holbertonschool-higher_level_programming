45f77afc9a19ed424c11f54d935fe29a1aff6b64
nitaly31 <geraldine.nitaly@gmail.com>
2021-10-04 21:26:01 -0700

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,51 +1,42 @@
 #!/usr/bin/python3
-""" Real definition of a rectangle """
+''' Real definition of a rectangle '''
 
 
 class Rectangle:
-    """ Defines a rectangle """
+    ''' Defines a rectangle '''
 
     def __init__(self, width=0, height=0):
-        """ Instantiation with optional width and height
-
-        Args:
-            width: width of the rectangle
-            height: height of the rectangle
-
-        """
-
+        ''' Instantiation with optional width and height '''
+        self.height = height
         self.width = width
-        self.height = heigth
 
     @property
     def width(self):
-        """ method that returns the value of the width and
-        private instance attribute
-
-        """
+        ''' method that returns the value of the width and
+        private instance attribute '''
 
         return self.__width
 
     @width.setter
-    def width(self, new_width):
-        """ method that calculates width  """
-        if type(new_width) is not int:
-            raise TypeError('width must be an integer')
-        if new_width < 0:
-            raise ValueError('width must be >= 0')
-        self.__width = new_width
+    def width(self, value):
+        ''' method that calculates width  '''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        """  method that returns the value of the height and
-        private instance attribute """
-        return self.__heigth
+        ''' method that returns the value of the height and
+        private instance attribute '''
+        return self.__height
 
     @height.setter
     def height(self, new_height):
-        """ method that calculates the height  """
+        ''' method that calculates the height '''
         if type(new_height) is not int:
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
         if new_height < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError("height must be >= 0")
         self.__height = new_height
