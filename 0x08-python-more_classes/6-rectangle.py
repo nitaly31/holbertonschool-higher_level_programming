ee461f041b7dfedea3873db6604027a87c5fb33b
nitaly31 <geraldine.nitaly@gmail.com>
2021-10-04 22:06:11 -0700

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -12,6 +12,7 @@ class Rectangle:
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
+
     @property
     def width(self):
         ''' method that returns the value of the width and
