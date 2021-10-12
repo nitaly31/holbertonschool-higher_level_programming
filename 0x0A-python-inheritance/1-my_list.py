f8fe802e91398562de4ccd51adb5abec89ac17a5
nitaly31 <geraldine.nitaly@gmail.com>
2021-10-11 19:38:24 -0700

    test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+'''My list '''
+
+
 class MyList(list):
     ''' class MyList that inherits from list '''
+
     def print_sorted(self):
         ''' prints the list '''
         print(sorted(self))
