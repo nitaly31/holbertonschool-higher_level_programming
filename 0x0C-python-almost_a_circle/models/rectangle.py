409995e1e85dd3442c76a34abd85965e85e2c8c7
nitaly31 <geraldine.nitaly@gmail.com>
2021-10-18 17:03:04 -0700

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -56,7 +56,7 @@ class Rectangle(Base):
         """ x setter """
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -70,6 +70,6 @@ class Rectangle(Base):
         """ y setter """
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("y must be >= 0")
         self.__y = value
