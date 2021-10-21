0ea599a0ee6919f96930bf6b5e0aa831791b8a6b
nitaly31 <geraldine.nitaly@gmail.com>
2021-10-21 11:30:24 -0700

    kwargs method in square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,24 +6,58 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    ''' Class Square inherits from Rectangle '''
+    '''
+    Class Square inherits from Rectangle
+    '''
 
     def __init__(self, size, x=0, y=0, id=None):
-        ''' Class constructor '''
+        '''
+        Class constructor
+        '''
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        ''' Size getter '''
+        '''
+        Size getter
+        '''
         return self.width
 
     @size.setter
     def size(self, value):
-        ''' Size setter '''
+        '''
+        Size setter
+        '''
         self.width = value
         self.height = value
 
     def __str__(self):
-        ''' The overloading __str__ method '''
+        '''
+        The overloading __str__ method
+        '''
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        '''
+        Assigns attributes
+        *args is the list of arguments - no-keyworded arguments
+        **kwargs can be thought of as a double pointer to a dictionary:
+        key/value (keyworded arguments)
+        **kwargs must be skipped if *args exists and is not empty
+        '''
+        arg_list = ["id", "size", "x", "y"]
+        if args and args[0] is not None:
+            for i in range(len(args)):
+                setattr(self, arg_list[i], arg[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        '''
+        Method that returs a dictionary with properties
+        '''
+        key_list = ["id", "size", "x", "y"]
+        value_list = [self.id, self.size, self.x, self.y]
+        return dict(zip(key_list, value_list))
