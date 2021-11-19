647808e2f536710dc7bd895246343eb36b26e942
nitaly31 <geraldine.nitaly@gmail.com>
2021-11-18 20:57:41 -0500

    creates the database hbtn_0d_usa and the table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY (id) REFERENCES states (id),
+    FOREIGN KEY (state_id) REFERENCES states (id),
     name VARCHAR(256) NOT NULL
 );
