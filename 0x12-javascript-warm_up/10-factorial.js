6cb9a9a0be81533d4d7e42444c7cbd6b45678dac
nitaly31 <geraldine.nitaly@gmail.com>
2021-12-14 17:47:25 -0500

    Update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function factorial (a) {
-	if ((Number.isNaN(a)) || (a === 1)) {
-		return 1;
-	}
-	return factorial(a - 1) * a;
+  if ((Number.isNaN(a)) || (a === 1)) {
+    return 1;
+  }
+  return factorial(a - 1) * a;
 }
 console.log(factorial(parseInt(process.argv[2])));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,14 +5,14 @@ let i;
 const arrayNumbers = [];
 
 for (i = 2; i < process.argv.length; i++) {
-	if (Number.isNaN(parseInt(process.argv[i])) === false) {
-		arrayNumbers[i - 2] = parseInt(process.argv[i]);
-	}
+  if (Number.isNaN(parseInt(process.argv[i])) === false) {
+    arrayNumbers[i - 2] = parseInt(process.argv[i]);
+  }
 }
 if (arrayNumbers.length > 1) {
-	biggest = Math.max.apply(null, arrayNumbers);
-	i = arrayNumbers.indexOf(biggest);
-	arrayNumbers[i] = -Infinity;
-	biggest = Math.max.apply(null, arrayNumbers);
+  biggest = Math.max.apply(null, arrayNumbers);
+  i = arrayNumbers.indexOf(biggest);
+  arrayNumbers[i] = -Infinity;
+  biggest = Math.max.apply(null, arrayNumbers);
 }
 console.log(biggest);
