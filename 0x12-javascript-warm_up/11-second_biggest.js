ecadeba6cc0c80f5abbc3214f08493c059d20e6f
nitaly31 <geraldine.nitaly@gmail.com>
2021-12-14 18:06:06 -0500

    Update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,9 @@
 #!/usr/bin/node
-
-let biggest = 0;
-let i;
-const arrayNumbers = [];
-
-for (i = 2; i < process.argv.length; i++) {
-  if (Number.isNaN(parseInt(process.argv[i])) === false) {
-    arrayNumbers[i - 2] = parseInt(process.argv[i]);
-  }
+let datos = 0;
+if (process.argv.length <= 3) {
+  console.log(datos);
+} else {
+  datos = process.argv.slice(2);
+  datos.sort(function (a, b) { return b - a; });
+  console.log(datos[1]);
 }
-if (arrayNumbers.length > 1) {
-  biggest = Math.max.apply(null, arrayNumbers);
-  i = arrayNumbers.indexOf(biggest);
-  arrayNumbers[i] = -Infinity;
-  biggest = Math.max.apply(null, arrayNumbers);
-}
-console.log(biggest);
