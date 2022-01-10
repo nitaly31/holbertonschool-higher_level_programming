3bd84a2c3dc7d870c9f372e9a0752c2cb88ea3c9
nitaly31 <geraldine.nitaly@gmail.com>
2022-01-09 23:44:27 -0500

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL as an argument,
 # sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s "$1" -H "X-School-User-Id: 98"
