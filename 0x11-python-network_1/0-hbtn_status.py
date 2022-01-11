b51f9e8128d6faa3a92fdfc2e042f814a62c1fd1
nitaly31 <geraldine.nitaly@gmail.com>
2022-01-10 17:27:33 -0800

    my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,3 @@ if __name__ == "__main__":
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode("utf-8")))
-        
