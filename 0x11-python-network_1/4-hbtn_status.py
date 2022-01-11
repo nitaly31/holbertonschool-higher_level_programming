8456a32e67a8e0d2149b4fafd0039e7b574c2cdc
nitaly31 <geraldine.nitaly@gmail.com>
2022-01-10 17:45:41 -0800

    my status #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" fetches https://intranet.hbtn.io/status"""
+
+import requests
+
+if __name__ == "__main__":
+
+    req = requests.get("https://intranet.hbtn.io/status")
+
+    print("Body response:")
+    print("\t- type: {}".format((type(req.text))))
+    print("\t- content: {}".format(req.text))
