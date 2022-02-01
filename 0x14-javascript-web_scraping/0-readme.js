b7743c332887ef51de5ccc9a36df7b8c2f9e7393
nitaly31 <geraldine.nitaly@gmail.com>
2022-02-01 23:28:48 +0000

    script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', errorFunc);
+
+function errorFunc (err, data) {
+    if(err){
+	console.log(err);
+    }
+    else {
+	console.log(data);
+    }
+}
